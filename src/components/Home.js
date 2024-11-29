edbbc8266a3436c84160ad6429314619ad1a9289
Aryan Patel <aryanpatel31206@gmail.com>
2024-11-29 14:27:09 -0500

    added thumbnail image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import logo from '../components/thumbnail.png'
 
 function Home() {
   return (
@@ -16,6 +17,7 @@ function Home() {
               <Card.Title>Meet Counter Conflicts</Card.Title>
               <Card.Text>
                 Try out our calendar which resolves scheduling conflicts..!
+                <img src={logo} alt="Logo"/>
               </Card.Text>
             </Card.Body>
           </Card>
