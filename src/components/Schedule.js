a4402b84a9de8da7cc805b6f85bca5548a8a59a5
Sean-Ye-Student <sean171171school@gmail.com>
2024-11-29 12:52:05 -0500

    Created a Calendar for the Schedule Tab

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
 export default function Schedule(){
+    const [value, onChange] = useState(new Date());
     return (
+        
         <div>
-          Schedule
+          <Calendar onChange={onChange} value={value}></Calendar>
         </div>
     )
 }
